f8a175b23b9af86a7458947942e5e2a800627617
Madusha Ravishani <mravishani7@gmail.com>
2020-10-22 23:17:37 +0530

    Updates

---
+++ precache-manifest.17c22478d6afe16167f7661f56b5a81e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d05bdc2b7b9b6f5da3d83c751f880be2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bcba451b7554f27911e4",
+    "url": "/static/css/2.3cf5d70f.chunk.css"
+  },
+  {
+    "revision": "ecafe5fa95d42f5c4070",
+    "url": "/static/css/main.90566059.chunk.css"
+  },
+  {
+    "revision": "bcba451b7554f27911e4",
+    "url": "/static/js/2.42156812.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.42156812.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecafe5fa95d42f5c4070",
+    "url": "/static/js/main.71aa513e.chunk.js"
+  },
+  {
+    "revision": "405a0d9530fd470eb51a",
+    "url": "/static/js/runtime-main.e562068f.js"
+  }
+]);
